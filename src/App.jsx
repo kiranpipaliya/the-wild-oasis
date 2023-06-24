59abc954740c96c7a4bf4600c2b5bda2915dfbd1
kiranpipaliya <kiranpipaliya446@gmail.com>
2023-06-24 12:21:20 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { styled } from "styled-components"
+
+const H1 = styled.h1`
+  font-size: 50px;
+  color: red;
+  font-weight: 500;
+`
+function App() {
+  return (
+    <H1>
+      Hello world 
+    </H1>
+  )
+}
+
+export default App
