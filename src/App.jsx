1eac41e217b17944d4da5fa2159b504f4f60c738
kiranpipaliya <kiranpipaliya446@gmail.com>
2023-07-09 17:38:22 +0530

    intigrate api and react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,69 @@
-import { styled } from "styled-components"
+import React from "react";
+import GlobalStyle from "./styles/GlobalStyle.js";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Account from "./pages/Account.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Login from "./pages/Login.jsx";
+import Settings from "./pages/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
-const H1 = styled.h1`
-  font-size: 50px;
-  color: red;
-  font-weight: 500;
-`
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <H1>
-      Hello world 
-    </H1>
-  )
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0",
+            color: "var(--color-grey-700",
+          },
+        }}
+      />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
